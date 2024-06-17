7f3eb1f8ed8d71783a37314ff12eb72a88193d74
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-17 20:22:17 +0530

    family
    testing

--- Day01/Examples/Solutions/Java/FamilyNames.java
+++ Day01/Examples/Solutions/Java/FamilyNames.java
@@ -1,7 +1,7 @@
 public class FamilyNames{
 
      public static void main(String []args){
-        System.out.println("MyName: saikiran reddy,");
+        System.out.println("MyName: saikiran ,");
         System.out.println("Father: bpnr,");
         System.out.println("Mother: mom");
      }
