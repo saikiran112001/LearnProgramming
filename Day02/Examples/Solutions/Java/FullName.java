4a542c1e0bf4ff845da2924969ebfbcda248e270
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-17 20:01:27 +0530

    full name
    testing

--- Day02/Examples/Solutions/Java/FullName.java
+++ Day02/Examples/Solutions/Java/FullName.java
@@ -1,8 +1,8 @@
 public class FullName{
 
      public static void main(String []args){
-        String first_name = "Prasanth Reddy";
-        String last_name = "M";
+        String first_name = "saikiran reddy";
+        String last_name = "B";
         System.out.println(first_name+" "+last_name);
      }
 }
