63c91bc60f02933c792a13d3af960047a11fa522
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-17 18:47:19 +0530

    my address
    testing

--- Day01/Exercises/Solutions/Java/MyAddress.java
+++ Day01/Exercises/Solutions/Java/MyAddress.java
@@ -1,7 +1,7 @@
 public class MyAddress{
 
      public static void main(String []args){
-        System.out.println("sai kiran reddy,");
+        System.out.println("bandi sai,");
         System.out.println("S/O M. ROSIREDDY");
         System.out.println("KULLURU VILLAGE,");
         System.out.println("KALUVOYA MANDAL,");
