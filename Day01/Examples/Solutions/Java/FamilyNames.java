bdc0b221355fca41d4f2569f15e0c0e64eee1ac1
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-17 20:29:27 +0530

    Update FamilyNames.java

--- Day01/Examples/Solutions/Java/FamilyNames.java
+++ Day01/Examples/Solutions/Java/FamilyNames.java
@@ -2,7 +2,7 @@ public class FamilyNames{
 
      public static void main(String []args){
         System.out.println("MyName: saikiran ,");
-        System.out.println("Father: bpnr,");
+        System.out.println("Father: bpnr reddy,");
         System.out.println("Mother: mom");
      }
 }
