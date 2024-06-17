c6de15ed576c3e3eff986ba606113d4d454a324a
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-17 21:18:20 +0530

    cities

---
+++ Day01/Examples/Solutions/Saikiran/Cities.java
@@ -0,0 +1,7 @@
+package Day01.Examples.Solutions.Saikiran;
+
+public class Cities {
+    public static void main(String[] args) {
+        System.out.println("nellore\ngudur\nrapur\ntirupathi\nvgr");
+    }
+}
