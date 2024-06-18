90c103a6ec34d7dff65408df67fe1f13b5568ddc
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-18 20:18:25 +0530

    defaultint

---
+++ Day02/Examples/Solutions/saikiran/DefaultInt.java
@@ -0,0 +1,8 @@
+package Day02.Examples.Solutions.saikiran;
+
+public class DefaultInt {
+    static int num;
+    public static void main(String[] args) {
+        System.out.println(num);
+    }
+}
