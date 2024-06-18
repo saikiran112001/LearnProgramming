c7e5d5b30750cd3355966e8413588f06a707e0a0
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-18 20:50:30 +0530

    defaultvalue

---
+++ Day02/Exercises/saikiran/DefaultValue.java
@@ -0,0 +1,30 @@
+package Day02.Exercises.saikiran;
+
+public class DefaultValue {
+    static int defaultInt;
+    static char defaultChar;
+    static byte defaultByte;
+    static short defaultShort;
+    static boolean defaultBoolean;
+    static long defaultLong;
+    static float defaultFloat;
+    static double defaultDouble; 
+
+    public static void main(String[] args) {
+       // int defaultInt;//
+        //char defaultChar;
+       // byte defaultByte;
+
+        System.out.println("int="+defaultInt);
+        System.out.println("char="+defaultChar);
+        System.out.println("byte="+defaultByte);
+        System.out.println("short="+defaultShort);
+        System.out.println("boolean="+defaultBoolean);
+        System.out.println("long="+defaultLong);
+        System.out.println("float="+defaultFloat);
+        System.out.println("double="+defaultDouble);
+        
+        
+
+    }
+}
