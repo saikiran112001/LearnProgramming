78f655ee79e2e595f053a1862ebcee1dea4b2093
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-18 19:31:56 +0530

    friend name

---
+++ Day01/Exercises/saikiran/FriendName.java
@@ -0,0 +1,9 @@
+package Day01.Exercises.saikiran;
+
+public class FriendName {
+    public static void main(String[] args) {
+        System.out.print("my name:sai kiran reddy");
+        System.out.print("\nfriend name:ravi reddy");
+    }
+    
+}
