5318af8a3cdec43f4ec512a1041a835688d9a17d
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-18 20:14:24 +0530

    is lght on

---
+++ Day02/Examples/Solutions/saikiran/IsLightOn.java
@@ -0,0 +1,8 @@
+package Day02.Examples.Solutions.saikiran;
+
+public class IsLightOn {
+    public static void main(String[] args) {
+        boolean is_light_On=true;
+        System.out.println("is_light-on=" +is_light_On);
+    }
+}
