9bf1de1e9cbe7f73def9b2ba7f8707912bedde16
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-18 19:58:43 +0530

    multiple ints

---
+++ Day02/Examples/Solutions/saikiran/MultipleInts.java
@@ -0,0 +1,10 @@
+package Day02.Examples.Solutions.saikiran;
+
+public class MultipleInts {
+    public static void main(String[] args) {
+        int a=10, b=20,c=30,d=40;
+        System.out.println("a="+a+"\nb="+b+"\nc="+c+"\nd="+d);
+
+    }
+    
+}
