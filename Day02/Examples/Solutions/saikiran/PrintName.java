d49b79d035dcd1d4be409f4551ef3d54e0600731
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-18 19:46:13 +0530

    print name

---
+++ Day02/Examples/Solutions/saikiran/PrintName.java
@@ -0,0 +1,8 @@
+package Day02.Examples.Solutions.saikiran;
+
+public class PrintName {
+    public static void main(String[] args) {
+        String name="sai kiran reddy";
+        System.out.print(name);
+    }
+}
