e62401560e40d86887df955217f7038dfaa95164
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-18 19:24:07 +0530

    Big letter

---
+++ Day01/Exercises/saikiran/BigLetter.java
@@ -0,0 +1,12 @@
+package Day01.Exercises.saikiran;
+
+public class BigLetter {
+    public static void main(String[] args) {
+        System.out.println("      SS SS");
+        System.out.println("   SS      ");
+        System.out.println("   SS      ");
+        System.out.println("      SS   ");
+        System.out.println("         SS");
+        System.out.println("   SS SS   ");
+    }
+}
