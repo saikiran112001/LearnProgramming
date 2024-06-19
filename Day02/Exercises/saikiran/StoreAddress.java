309fb41fee7967cfea118650963f2631d08a78ef
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-19 10:37:37 +0530

    address

---
+++ Day02/Exercises/saikiran/StoreAddress.java
@@ -0,0 +1,8 @@
+package Day02.Exercises.saikiran;
+
+public class StoreAddress {
+public static void main(String[] args) {
+        String my_address="#44 5th cross jp nagar 6th phase 2nd floor kr layout bangalore 560078";
+        System.out.println("my_address="+my_address);
+    }
+}
