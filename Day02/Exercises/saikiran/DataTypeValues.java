832684d97546ac56d147d42c4339550dcfa49aa4
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-19 10:14:40 +0530

    datatypes values

---
+++ Day02/Exercises/saikiran/DataTypeValues.java
@@ -0,0 +1,24 @@
+package Day02.Exercises.saikiran;
+
+public class DataTypeValues {
+    static int a=100;
+    static byte b=10;
+    static short s=500;
+    static long l=1000000;
+    static float f=3.14f;
+    static double d=2.71828d;
+    static char ch='A';
+    static boolean bo=true;
+
+    public static void main(String args[]){
+        
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        System.out.println("s="+s);
+        System.out.println("l="+l);
+        System.out.println("f="+f);
+        System.out.println("d="+d);
+        System.out.println("ch="+ch);
+        System.out.println("bo="+bo);
+    }
+}
