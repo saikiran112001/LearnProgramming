7dd19fdd1c2a233f77acd7c2783dc522e360ba72
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-19 18:31:00 +0530

    unaryop

---
+++ Day03/Examples/saikiran/UnaryOperators.java
@@ -0,0 +1,14 @@
+package Day03.Examples.saikiran;
+
+public class UnaryOperators {
+    public static void main(String args[]){
+        int i=5;
+        i++;
+        System.out.println(i);
+        ++i;
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i++);
+        System.out.println(i);
+    }
+}
