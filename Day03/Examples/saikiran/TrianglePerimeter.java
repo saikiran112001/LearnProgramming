7183341aa0d5fc96560a761d0b17f6980a6969ac
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-19 12:09:34 +0530

    triangle

---
+++ Day03/Examples/saikiran/TrianglePerimeter.java
@@ -0,0 +1,14 @@
+package Day03.Examples.saikiran;
+
+public class TrianglePerimeter {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+
+       int  perimeter=a+b+c;
+   
+    System.out.println("perimeter_of_triangle="+perimeter);
+
+    }
+}
