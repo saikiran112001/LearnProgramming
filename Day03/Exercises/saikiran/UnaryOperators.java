4ec0771b7148e9cbf60ba4690b4180ff45315428
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-20 12:26:15 +0530

    unary Op

---
+++ Day03/Exercises/saikiran/UnaryOperators.java
@@ -0,0 +1,14 @@
+package Day03.Exercises.saikiran;
+
+public class UnaryOperators {
+    public static void main(String[] args) {
+        int i=10;
+        i++;i++;++i;
+
+        int j=i++;
+
+        System.out.println(j);
+        
+
+    }
+}
