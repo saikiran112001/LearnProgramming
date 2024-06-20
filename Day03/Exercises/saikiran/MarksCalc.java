81a072e7c18532676ba1d3d6475b00022a2ee936
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-20 11:25:48 +0530

    marks total

---
+++ Day03/Exercises/saikiran/MarksCalc.java
@@ -0,0 +1,15 @@
+package Day03.Exercises.saikiran;
+
+public class MarksCalc {
+    public static void main(String[] args) {
+        int m1=88;
+        int m2=73;
+        int m3=88;
+         
+        int total_marks=m1+m2+m3;
+        double percentage=(total_marks/3.0);
+
+        System.out.println("total marks="+total_marks);
+        System.out.println("percentage="+percentage+"%");
+    }
+}
