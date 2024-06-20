0d576e5843d3bbe16b446b0a01e972ce3fcbf010
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-20 10:59:16 +0530

    rectangleOp

---
+++ Day03/Exercises/saikiran/RectangleOP.java
@@ -0,0 +1,15 @@
+package Day03.Exercises.saikiran;
+
+public class RectangleOP {
+    public static void main(String[] args) {
+        
+    int length=4;
+    int breadth=9;
+
+    int area=length+breadth;
+    int perimeter=2*(length+breadth);
+
+    System.out.println("Area of rectangle="+area);
+    System.out.println("Area of peremeter="+perimeter);
+}
+}
