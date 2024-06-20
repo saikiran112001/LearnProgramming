961fd8bc527e1dc9312500e79a5110f01269cb88
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-20 13:07:37 +0530

    assignop

---
+++ Day04/Examples/saikiran/Assignmentop.java
@@ -0,0 +1,16 @@
+package Day04.Examples.saikiran;
+
+public class Assignmentop {
+    public static void main(String[] args) {
+        int a=4;
+        System.out.println("a="+a);
+        a+=a;
+        System.out.println("a="+a);
+        a=7+a;
+        System.out.println("a="+a);   
+        a*=a;
+        System.out.println("a="+a);
+        a/=2;
+        System.out.println("a="+a);
+    }
+}
