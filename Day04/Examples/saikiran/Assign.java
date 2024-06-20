b43159393bac1a9998517b029e240d623715fad8
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-20 13:22:13 +0530

    assignOP

---
+++ Day04/Examples/saikiran/Assign.java
@@ -0,0 +1,12 @@
+package Day04.Examples.saikiran;
+
+public class Assign {
+    
+    public static void main(String[] args) {
+      int s=10;
+      int a=s;
+      s+=10;
+      s=s+10;
+     System.out.println(s+","+a);
+    }
+}
