ca9fb2791f7771f539a283b91c75e67d433a306f
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-21 10:51:08 +0530

    relational

---
+++ Day04/Exercises/saikiran/RElationalOP.java
@@ -0,0 +1,14 @@
+package Day04.Exercises.saikiran;
+
+public class RElationalOP {
+    public static void main(String[] args) {
+        float f = 75.0f;
+        double d = 75.0;
+        int i = 75;
+        
+        System.out.println("f and d are equal? - " + (f==d));
+        System.out.println("f and i are equal? - " + (f==i));
+        System.out.println("d and i are equal? - " + (d==i));
+
+    }
+}
