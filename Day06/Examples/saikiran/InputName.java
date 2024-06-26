15bedb4651dc9b3080c3de38b4ed06a99346f4e6
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-26 22:41:03 +0530

    input name

---
+++ Day06/Examples/saikiran/InputName.java
@@ -0,0 +1,13 @@
+package Day06.Examples.saikiran;
+import java.util.Scanner;
+
+public class InputName {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your name");
+        String name=sc.nextLine();
+        System.out.println("my name is:"+name);
+        sc.close();
+    }
+    
+}
