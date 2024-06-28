deeef25e15e3bde28ac7980f82be88ba1f3523dd
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-06-28 11:26:49 +0530

    unaryop

---
+++ Day06/Examples/saikiran/UnaryOP.java
@@ -0,0 +1,21 @@
+package Day06.Examples.saikiran;
+import java.util.Scanner;
+public class UnaryOP {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter the value of i");
+        int i=sc.nextInt();
+
+        i++;
+        System.out.println(i);
+        ++i;
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i++);
+        System.out.println(i);
+
+        sc.close();
+    }
+    
+}
