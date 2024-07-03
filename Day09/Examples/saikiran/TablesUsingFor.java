57b16ba93dbd858f7e57746c69eddced200750c3
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-07-03 22:24:01 +0530

    tables using for

---
+++ Day09/Examples/saikiran/TablesUsingFor.java
@@ -0,0 +1,10 @@
+package Day09.Examples.saikiran;
+
+public class TablesUsingFor {
+    public static void main(String args[]){
+        int n=2;
+        for(int i=1;i<=10;i++){
+            System.out.println(n+"*"+i+"="+(n*i));
+        }
+    }
+}
