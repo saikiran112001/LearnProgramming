95ba6336aa7ce12d41a33ecf4081cd37ad7a07a8
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-07-03 23:03:07 +0530

    tabels using while

---
+++ Day09/Examples/saikiran/TablesUsingWhile.java
@@ -0,0 +1,14 @@
+package Day09.Examples.saikiran;
+public class TablesUsingWhile {
+    public static void main(String args[])
+    {
+        int n=5;
+        int i=1;
+        while(i<=10)
+        {
+            System.out.println(n+"X"+i+"="+(n*i));
+            i++;
+        }
+    }
+    
+}
