3a04691049033a02c7473fbc27507106e7f42808
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-07-04 11:25:59 +0530

    print odd

---
+++ Day09/Examples/saikiran/PrintOddNumbers.java
@@ -0,0 +1,13 @@
+package Day09.Examples.saikiran;
+
+public class PrintOddNumbers {
+    public static void main(String[] args) 
+    {
+        for (int i = 1; i <= 10; i += 2) 
+        {
+            System.out.println(i);
+        }
+    }
+}
+
+
