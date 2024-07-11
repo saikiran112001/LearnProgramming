908899f25a464a261d478529124755e68fd0ab7a
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-07-11 22:59:48 +0530

    pattern one

---
+++ Day09/Exercises/saikiran/PatterOne.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PatterOne {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i,j,n=10;
+
+        for(j=1;j<=4;j++)
+        {
+        for(i=1;i<=n;i++)
+        {
+            System.out.print("*");
+            
+        }
+        System.out.println();
+        }
+    sc.close();
+    
+}
+}
