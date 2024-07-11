f87df8f149079aee4918ef1ae9e9a911e1864077
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-07-11 17:18:01 +0530

    fibonac series

--- Day09/Exercises/saikiran/FibonacciSeries.java
+++ Day09/Exercises/saikiran/FibonacciSeries.java
@@ -6,14 +6,13 @@ public class FibonacciSeries {
         System.out.println("enter the fibo number");
         int num=sc.nextInt();
 
-        System.out.print("fibo series of %d terms");
         int a=0,b=1;
-
         System.out.println(a+" "+b+" ");
+        
         for(int i=2;i<num;i++)
         {
-            int next=a+b;
-            System.out.println(next +" ");
+            int c=a+b;
+            System.out.println(c +" ");
             a=b;
             b=num;
         }
