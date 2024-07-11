b2a9698b88bc67fa0856b558caec895c0da368d9
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-07-11 10:00:29 +0530

    armstrong

--- Day09/Exercises/saikiran/AmstrongNumbers.java
+++ Day09/Exercises/saikiran/AmstrongNumbers.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 public class AmstrongNumbers {
     public static void main(String[] args){
-        int i=1,a,arm,n;
+        int i=1,a,sum,n;
 
         System.out.println("Armstrongnumber between 1 and 500");
 
         while(i<500)
         {
             n=i;
-            arm=0;
+            sum=0;
             while(n>0)
             {
                 a=n%10;
-                arm=arm+(a*a*a);
+                sum=sum+(a*a*a);
                 n=n/10;
 
             }
-            if(arm==i)
+            if(sum==i)
             System.out.println(i);
             i++;
         }
