950892cd8175f660519ebb83e38f190533d81a9b
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-07-18 12:11:06 +0530

    array four

---
+++ Day10/Examples/saikiran/ArrayFour.java
@@ -0,0 +1,9 @@
+package Day10.Examples.saikiran;
+
+public class ArrayFour {
+    public static void main(String[] args){
+        String[] computer = {"RAM","HDD","MOUSE"};
+		String[] parts = {computer[0],computer[2]};
+		System.out.print(parts[1]);
+    }
+}
