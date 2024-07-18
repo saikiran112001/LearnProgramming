8b0c3a80bd1261e8a8bb295a7d10960a4f9149e9
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-07-18 12:34:11 +0530

    array six

---
+++ Day10/Examples/saikiran/ArraySix.java
@@ -0,0 +1,13 @@
+package Day10.Examples.saikiran;
+
+public class ArraySix {
+    public static void main(String args[]){
+        String[] ary = {"KITE", "AIR"};
+		String str = "PLANE";
+		ary[1] = str;
+	    str = "FLY";
+        ary[1] = str;
+		System.out.println(ary[1]);
+
+    }
+}
