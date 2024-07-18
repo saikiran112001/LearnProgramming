72a8597188858e6966236a22dd4771c0c2c4567b
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-07-18 19:18:12 +0530

    reverse array

---
+++ Day10/Examples/saikiran/ReverseArray.java
@@ -0,0 +1,37 @@
+package Day10.Examples.saikiran;
+
+import java.util.Scanner;
+public class ReverseArray {
+
+    public static void main(String array[]){
+        Scanner sc=new Scanner(System.in);
+
+        int a[]=new int[5];
+        int b[]=new int[5];
+        int i,j;
+        
+
+        for(i=0;i<a.length;i++)
+        {
+            
+            a[i]=sc.nextInt();
+        }
+        
+        i=b.length-1;
+        
+        for(j=0;j<b.length;j++)
+        {
+            
+            b[j]=a[i];
+            i--;
+            
+         }
+         for(i=0;i<b.length;i++){
+            System.out.print(b[i]);
+         }
+        sc.close();
+        
+
+    }
+    
+}
