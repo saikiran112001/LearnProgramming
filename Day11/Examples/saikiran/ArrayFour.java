51ef7e062c7f5409cf03fda6cb0a658ddb6afff7
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-07-19 22:46:36 +0530

    array four

--- Day11/Examples/saikiran/ArrayFour.java
+++ Day11/Examples/saikiran/ArrayFour.java
@@ -2,12 +2,13 @@ package Day11.Examples.saikiran;
 
 public class ArrayFour {
     public static void main(String[] args) {
-        int[][] m = { {1,1,1,1},{1,2,3,4},{2,2,2,2},{2,4,6,8} };            
-        int sum = 0;
-        for (int k = 0; k < m.length; k++) {
-            sum = sum + m[m.length-1-k][1];
-        }
-        System.out.println(sum);
+
+    int[][] m = { {1,1,1,1},{1,2,3,4},{2,2,2,2},{2,4,6,8} };            
+    int sum = 0;
+    for (int k = 0; k < m.length; k++) {
+    sum = sum + m[m.length-1-k][1];
+    }
+    System.out.println(sum);
     }
     
 }
