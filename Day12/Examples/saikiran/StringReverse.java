f4f0f3da6d8575c57c93f9871af67a2e8933b0a3
saikiran112001 <reddysaikiran.bandi@gmail.com>
2024-07-22 19:59:47 +0530

    string reverse

---
+++ Day12/Examples/saikiran/StringReverse.java
@@ -0,0 +1,19 @@
+package Day12.Examples.saikiran;
+
+import java.util.Scanner;
+public class StringReverse {
+    public static void main (String args[]){
+        Scanner sc=new Scanner (System.in);
+
+        System.out.println("enter string value");
+        String s=sc.nextLine();
+        String rev="";
+        int i;
+        for(i=s.length()-1;i>=0;i--){
+           rev+=s.charAt(i);
+           //System.out.print(rev);
+        }  
+        System.out.print("reversed string="+rev);
+        sc.close();
+}
+}
